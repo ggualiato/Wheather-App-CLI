c0b7888a403bb2bede312a2bb562c1f1dd856060
ggualiato <ggualiato@gmail.com>
2017-11-13 23:33:38 -0200

    add get currenty location based on ip

--- app-extra.js
+++ app-extra.js
@@ -1,6 +1,4 @@
 const yargs = require('yargs');
-const axios = require('axios');
-const fs = require('fs');
 
 const services = require('./extra-features/services.js');
 
@@ -30,66 +28,15 @@ const argv = yargs
 .argv;
 
 if (argv.default) {
-  try {
 
-    var location = services.getDefaultLocation();
+  services.showDefaultLocation();
 
-    console.log(location.address);
-
-    var weatherUrl = `https://api.darksky.net/forecast/ae7f0513dd99ec226c1e6d981bf2697a/${location.latitude},${location.longitude}?units=si`;
-
-    axios.get(weatherUrl).then((response) => {
-      var temperature = response.data.currently.temperature;
-      var apparentTemperature = response.data.currently.apparentTemperature;
-      var weatherType = response.data.currently.precipType;
-
-      console.log(`${weatherType} | Temperature: ${temperature} | Apparent Temperature: ${apparentTemperature} |`);
-
-    }).catch((e) => {
-      if (e === 'ENOTFOUND'){
-        console.log('Unable to connect to API servers.');
-      } else {
-        console.log(e.message)
-      }
-    });
-  } catch (e) {
-    console.log('There is no default location');
-  }
 } else if (argv.address) {
-  var encodedAddress = encodeURIComponent(argv.address);
-  var geocodeUrl = `http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`;
 
-  axios.get(geocodeUrl).then((response) => {
-    if (response.data.status === 'ZERO_RESULTS'){
-      throw new Error('Unable to find that address')
-    }
+  services.showWeather(argv.address, argv.setdefault);
 
-    var formatted_address = response.data.results[0].formatted_address;
-    var lat = response.data.results[0].geometry.location.lat;
-    var lng = response.data.results[0].geometry.location.lng;
-    var weatherUrl = `https://api.darksky.net/forecast/ae7f0513dd99ec226c1e6d981bf2697a/${lat},${lng}?units=si`;
+} else {
 
-    console.log(formatted_address);
+  services.getCurrentLocation();
 
-    if (argv.setdefault & response.data.status === 'OK') {
-
-      console.log('Setting default location...');
-
-      services.saveLocation(lat, lng, formatted_address);
-
-    }
-
-    return axios.get(weatherUrl);
-  }).then((response) => {
-    var temperature = response.data.currently.temperature;
-    var apparentTemperature = response.data.currently.apparentTemperature;
-
-    console.log(`It's currently ${temperature}. It feels like ${apparentTemperature}.`);
-  }).catch((e) => {
-    if (e === 'ENOTFOUND'){
-      console.log('Unable to connect to API servers.');
-    } else {
-      console.log(e.message)
-    }
-  });
 }
--- extra-features/services.js
+++ extra-features/services.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
+const axios = require('axios');
 
 var geocodeUrl = `http://maps.googleapis.com/maps/api/geocode/json?address=`;
 var weatherUrl = `https://api.darksky.net/forecast/ae7f0513dd99ec226c1e6d981bf2697a/`;
+var ipApiUrl = 'http://ip-api.com/json';
 
 var getDefaultLocation = () => {
   var defaultString = fs.readFileSync('weather-data.json');
@@ -20,7 +22,100 @@ var saveLocation = (lat, lng, formatted_address) => {
   fs.writeFileSync('weather-data.json', JSON.stringify(location));
 }
 
+var showDefaultLocation = () => {
+  try {
+
+    var location = getDefaultLocation();
+
+    var url = `${weatherUrl}${location.latitude},${location.longitude}?units=si`;
+
+    console.log(location.address);
+
+    axios.get(url).then((response) => {
+      var temperature = response.data.currently.temperature;
+      var apparentTemperature = response.data.currently.apparentTemperature;
+
+      console.log(`Temperature: ${temperature} | Apparent Temperature: ${apparentTemperature} |`);
+
+    }).catch((e) => {
+      if (e === 'ENOTFOUND'){
+        console.log('Unable to connect to API servers.');
+      } else {
+        console.log(e.message);
+      }
+    });
+  } catch (e) {
+    console.log('There is no default location');
+  }
+}
+
+var showWeather = (address, setdefault) => {
+  var encodedAddress = encodeURIComponent(address);
+  var url = `${geocodeUrl}${encodedAddress}`;
+
+  axios.get(url).then((response) => {
+    if (response.data.status === 'ZERO_RESULTS'){
+      throw new Error('Unable to find that address')
+    }
+
+    var formatted_address = response.data.results[0].formatted_address;
+    var lat = response.data.results[0].geometry.location.lat;
+    var lng = response.data.results[0].geometry.location.lng;
+    var url = `${weatherUrl}${lat},${lng}?units=si`;
+
+    console.log(formatted_address);
+
+    if (setdefault && response.data.status === 'OK') {
+
+      console.log('Setting default location...');
+
+      saveLocation(lat, lng, formatted_address);
+    }
+
+    return axios.get(url);
+
+  }).then((response) => {
+    var temperature = response.data.currently.temperature;
+    var apparentTemperature = response.data.currently.apparentTemperature;
+
+    console.log(`Temperature: ${temperature} | Apparent Temperature: ${apparentTemperature} |`);
+
+  }).catch((e) => {
+    if (e === 'ENOTFOUND'){
+      console.log('Unable to connect to API servers.');
+    } else {
+      console.log(e.message);
+    }
+  });
+}
+
+var getCurrentLocation = () => {
+  axios.get(ipApiUrl).then((response) => {
+    var lat = response.data.lat;
+    var lng = response.data.lon;
+    var url = `${weatherUrl}${lat},${lng}?units=si`;
+
+    return axios.get(url);
+
+  }).then((response) => {
+    var temperature = response.data.currently.temperature;
+    var apparentTemperature = response.data.currently.apparentTemperature;
+
+    console.log('Currenty Location');
+    console.log(`Temperature: ${temperature} | Apparent Temperature: ${apparentTemperature} |`);
+  }).catch((e) => {
+    if (e === 'ENOTFOUND') {
+      console.log('Unable to connect to API servers.');
+    } else {
+      console.log(e.message);
+    }
+  });
+}
+
 module.exports = {
   getDefaultLocation,
-  saveLocation
+  saveLocation,
+  showDefaultLocation,
+  showWeather,
+  getCurrentLocation
 };
